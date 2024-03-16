9e6df804ef18ce41bbac7b07cbb5718c0d7202c9
Krishil Rohit Sheth <97043368+krishilsheth404@users.noreply.github.com>
2024-03-16 20:47:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ app.use(express.json());
 app.use("/customer", customerRoute);
 
 mongoose
-  .connect("url", (req, res) => {
-    console.log("Connected to MongoDB");
-  })
+  .connect("mongodb+srv://krishilsheth:JT98v8Vyxd4FBQHZ@mmbackend.no7pcgx.mongodb.net/mannmilaap?retryWrites=true&w=majority&appName=mmbackend")
   .then(() => {
+    console.log("Connected to DB")
     app.listen(3000, () => {
       console.log("Server running on port 3000");
     });
