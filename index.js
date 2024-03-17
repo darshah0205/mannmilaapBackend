c24bc785becba04c221cb53a45e21c6ed7e983c3
darshit <darshit.hs@somaiya.edu>
2024-03-17 15:12:33 +0530

    change of port number for convenience

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); // Import the cors middleware
 const customerRoute = require("./routes/customer.route.js");
 
 const app = express();
 app.use(express.json());
 
+// Use the cors middleware
+app.use(cors());
+
 app.use("/customer", customerRoute);
 
 mongoose
-  .connect("mongodb+srv://krishilsheth:JT98v8Vyxd4FBQHZ@mmbackend.no7pcgx.mongodb.net/mannmilaap?retryWrites=true&w=majority&appName=mmbackend")
+  .connect(
+    "mongodb+srv://krishilsheth:JT98v8Vyxd4FBQHZ@mmbackend.no7pcgx.mongodb.net/mannmilaap?retryWrites=true&w=majority&appName=mmbackend"
+  )
   .then(() => {
-    console.log("Connected to DB")
-    app.listen(3000, () => {
+    console.log("Connected to DB");
+    app.listen(3001, () => {
       console.log("Server running on port 3000");
     });
   })
